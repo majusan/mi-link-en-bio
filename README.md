53cec380c761cfd4d1df22d272ddd09d8e655cd2
majusan <48169915+majusan@users.noreply.github.com>
2025-07-07 23:49:42 -0300

    Initial commit: Link in Bio project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# mi-link-en-bio
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Matias-Fernandez-the-vuer/pen/KwdPPRP](https://codepen.io/Matias-Fernandez-the-vuer/pen/KwdPPRP).
+
---
+++ script.js
@@ -0,0 +1,5 @@
+// No se requiere JavaScript adicional para el funcionamiento básico de los enlaces
+// ni para el widget de Elfsight, ya que su script se carga de forma asíncrona en el HTML.
+// Puedes añadir aquí cualquier funcionalidad interactiva que desees para tu página.
+
+console.log("¡Tu mini-página está completa y lista!");
